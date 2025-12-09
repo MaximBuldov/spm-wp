58232e26d9467fa58a9ebf356cf1fc8be166e311
Maksim Buldau <maxbuldov@gmail.com>
2025-12-09 15:13:25 -0800

    add users in response

--- wp-content/plugins/spm/spm.php
+++ wp-content/plugins/spm/spm.php
@@ -13,6 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'SPM_PATH', plugin_dir_path( __FILE__ ) );
 
 require SPM_PATH . 'inc/auth.php';
+require SPM_PATH . 'inc/auth-v2.php';
 require SPM_PATH . 'inc/works.php';
 require SPM_PATH . 'inc/users.php';
 require SPM_PATH . 'inc/options.php';
