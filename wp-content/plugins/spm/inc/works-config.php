551f04046100e2f157365cd39bd20ad593c25524
Maksim Buldau <maxbuldov@gmail.com>
2025-12-11 15:22:16 -0800

    create revision

--- wp-content/plugins/spm/inc/works-config.php
+++ wp-content/plugins/spm/inc/works-config.php
@@ -1,18 +1,24 @@
 <?php
-add_action('acf/save_post', function( $post_id ) {
-    if ( ! is_numeric( $post_id ) ) {
-        return;
+add_filter('rest_pre_insert_works', function( $prepared_post, $request, $creating ) {
+    if ( $creating ) {
+        return $prepared_post;
+    }
+
+    $method = $request->get_method();
+    if ( $method !== 'PUT' && $method !== 'PATCH' ) {
+        return $prepared_post;
+    }
+
+    $post_id = isset($prepared_post->ID) ? (int)$prepared_post->ID : 0;
+    if ( ! $post_id ) {
+        return $prepared_post;
     }
 
     if ( wp_is_post_revision( $post_id ) ) {
-        return;
-    }
-
-    $post_type = get_post_type( $post_id );
-    if ( $post_type !== 'works' ) {
-        return;
+        return $prepared_post;
     }
 
     wp_save_post_revision( $post_id );
 
-}, 20);
+    return $prepared_post;
+}, 10, 3);
