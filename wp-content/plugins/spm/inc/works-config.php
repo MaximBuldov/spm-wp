b0c305bcdf7995ae74f22c110a2048832e785df7
Maksim Buldau <maxbuldov@gmail.com>
2026-03-12 17:47:37 -0700

    restrict works api

--- wp-content/plugins/spm/inc/works-config.php
+++ wp-content/plugins/spm/inc/works-config.php
@@ -21,20 +21,3 @@ add_action('rest_after_insert_works', function ( $post, $request, $creating ) {
     update_field('work_id', $post_id, $history_id);
     update_field('snapshot', wp_json_encode($snapshot), $history_id);
 }, 20, 3);
-
-function restrict_works_rest_api($result, $server, $request) {
-    $route = $request->get_route();
-    
-    if (strpos($route, '/wp/v2/works') !== false) {
-        if (!is_user_logged_in()) {
-            return new WP_Error(
-                'rest_forbidden',
-                'You must be logged in to access works.',
-                ['status' => 401]
-            );
-        }
-    }
-    
-    return $result;
-}
-add_filter('rest_pre_dispatch', 'restrict_works_rest_api', 10, 3);
